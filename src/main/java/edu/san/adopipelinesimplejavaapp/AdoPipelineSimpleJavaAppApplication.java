430b8523c3f0b5c7fea9fdd656413564eb910839
Sankar <san.gct@gmail.com>
2020-04-09 01:06:28 -0400

    Initial Commit

---
+++ src/main/java/edu/san/adopipelinesimplejavaapp/AdoPipelineSimpleJavaAppApplication.java
@@ -0,0 +1,13 @@
+package edu.san.adopipelinesimplejavaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoPipelineSimpleJavaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdoPipelineSimpleJavaAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/san/adopipelinesimplejavaapp/AdoPipelineSimpleJavaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.san.adopipelinesimplejavaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoPipelineSimpleJavaAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
